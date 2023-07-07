d6cdde193fa36ce8b1e6f3b968aa2cee2408a451
sipra <sipramishra085@gmail.com>
2023-07-07 15:28:52 +0530

    [Sipra]Add:Check for a option using Random

--- SnakeAndLadderProgram/SnakeAndLadder.cs
+++ SnakeAndLadderProgram/SnakeAndLadder.cs
@@ -8,13 +8,43 @@ namespace SnakeAndLadderProgram
 {
     public class SnakeAndLadder
     {
+       
+        public const int CaseOfNoPlay = 0;
+        public const int CaseOfNoLadder = 1;
+        public const int CaseOfSnake = 2;
         public void RollingDice()
         {
-            Console.WriteLine("Start the Snake and Ladder Game");
+           
             Console.WriteLine("Single player at start Position 0");
             Random random = new Random();
             int RollDice = random.Next(1, 6);
             Console.WriteLine("Player rolls the dice and get: " + RollDice);
+            Random random1 = new Random();
+            int option = random1.Next(2);
+            int position = 0;
+
+
+            switch (option)
+            {
+                case CaseOfNoPlay:
+                    {
+                        Console.WriteLine("No Play " + position);
+                        Console.WriteLine("Player stay at same position-");
+                        break;
+                    }
+                case CaseOfNoLadder:
+                    {
+                        Console.WriteLine("Ladder " + (RollDice + position));
+                        Console.WriteLine("Player move ahead {0} steps ", RollDice);
+                        break;
+                    }
+                case CaseOfSnake:
+                    {
+                        Console.WriteLine("Snake " + (RollDice - position));
+                        Console.WriteLine("Player move back {0} steps ", RollDice);
+                        break;
+                    }
+            }
         }
     }
 }

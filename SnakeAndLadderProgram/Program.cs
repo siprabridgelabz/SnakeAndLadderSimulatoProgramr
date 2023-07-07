7090bdc556f02df78dee311c0156354a36a6145b
sipra <sipramishra085@gmail.com>
2023-07-07 15:11:22 +0530

    [Sipra]Add:Played with single player at start position 0

---
+++ SnakeAndLadderProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Snake and Ladder Program");
+            SnakeAndLadder snakeLadder = new SnakeAndLadder();
+            snakeLadder.RollingDice();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SnakeAndLadderProgram/SnakeAndLadder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProgram
+{
+    public class SnakeAndLadder
+    {
+        public void RollingDice()
+        {
+            Console.WriteLine("Start the Snake and Ladder Game");
+            int position = 0;
+            Console.WriteLine("Single player at start Position 0");
+        }
+    }
+}

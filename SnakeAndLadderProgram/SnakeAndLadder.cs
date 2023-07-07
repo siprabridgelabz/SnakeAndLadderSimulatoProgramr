4b5e1ba625c1287d55caa3ca598b81704b08694a
sipra <sipramishra085@gmail.com>
2023-07-07 15:38:02 +0530

    [Sipra]Add:Check winning position till 100

--- SnakeAndLadderProgram/SnakeAndLadder.cs
+++ SnakeAndLadderProgram/SnakeAndLadder.cs
@@ -8,42 +8,57 @@ namespace SnakeAndLadderProgram
 {
     public class SnakeAndLadder
     {
-       
         public const int CaseOfNoPlay = 0;
-        public const int CaseOfNoLadder = 1;
+        public const int CaseOfLadder = 1;
         public const int CaseOfSnake = 2;
+        public const int MaxPosition = 100;
+
         public void RollingDice()
         {
            
-            Console.WriteLine("Single player at start Position 0");
-            Random random = new Random();
-            int RollDice = random.Next(1, 6);
-            Console.WriteLine("Player rolls the dice and get: " + RollDice);
-            Random random1 = new Random();
-            int option = random1.Next(2);
+            Console.WriteLine("Single player at starting position 0");
+            Console.WriteLine(" ");
             int position = 0;
-
-
-            switch (option)
+            while (position != MaxPosition)
             {
-                case CaseOfNoPlay:
-                    {
-                        Console.WriteLine("No Play " + position);
-                        Console.WriteLine("Player stay at same position-");
-                        break;
-                    }
-                case CaseOfNoLadder:
-                    {
-                        Console.WriteLine("Ladder " + (RollDice + position));
-                        Console.WriteLine("Player move ahead {0} steps ", RollDice);
-                        break;
-                    }
-                case CaseOfSnake:
-                    {
-                        Console.WriteLine("Snake " + (RollDice - position));
-                        Console.WriteLine("Player move back {0} steps ", RollDice);
-                        break;
-                    }
+                Random random = new Random();
+                int RollDice = random.Next(1, 6);
+                Console.WriteLine("Player rolls the die and get: " + RollDice);
+                Random random1 = new Random();
+                int option = random1.Next(3);
+
+                switch (option)
+                {
+                    case CaseOfNoPlay:
+                        {
+                            Console.WriteLine("Option NO PLAY selected " + position);
+                            Console.WriteLine("Player stey at same postion");
+                            break;
+                        }
+
+                    case CaseOfLadder:
+                        {
+                            Console.WriteLine("Option LADDER selected " + position);
+                            Console.WriteLine("Player move ahead " + RollDice);
+                            position = RollDice + position;
+                            break;
+                        }
+
+                    case CaseOfSnake:
+                        {
+                            Console.WriteLine("Option SNAKE is selected " + (RollDice + position));
+                            Console.WriteLine("Player move behind by " + RollDice);
+                            position = position - RollDice;
+                            break;
+                        }
+                }
+                if (position < 0)
+                {
+                    position = 0;
+                    Console.WriteLine("Player have to restart from position " + position);
+                }
+                Console.WriteLine("Current position of player is " + position);
+                Console.WriteLine(" ");
             }
         }
     }
